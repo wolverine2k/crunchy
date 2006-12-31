00e35cc3ab4213dd844c4d483d1e7add3074173a
johannes.wollard <johannes.wollard@aa0cd930-251a-0410-a94a-f18be9da9e77>
2006-12-31 23:27:41 +0000

    importing the classroom server

---
+++ branches/johann/classroom/constants.py
@@ -0,0 +1,16 @@
+"""constants for the classroom server"""
+
+# the operation worked!
+SUCCESS = 0
+
+# only root can do that
+ONLY_ROOT = 1
+
+# nope, still locked out:
+INVALID_LOGIN = 2
+
+# try changing your name:
+UNAME_ALREADY_EXISTS = 3
+
+# not allowed, sorry:
+NO_ACCESS = 4
