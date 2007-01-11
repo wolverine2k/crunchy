eaa9ace75c84f6a48af8ed759a2aaaf6787e003c
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2007-01-11 17:13:53 +0000

    Creating merging directory for integrating crunchy and chewy.

---
+++ branches/andre/merge/src/configuration.py
@@ -0,0 +1,163 @@
+""" configuration.py:
+    Keeps track of user preferences.
+    In future we should provides a GUI for changing the preferences.
+"""
+
+import os
+from ConfigParser import SafeConfigParser
+from element_tree import HTMLTreeBuilder
+import translation
+
+class Borg(object):
+    """From the 2nd edition of the Python cookbook.
+       Ensures that all instances share the same state and behaviour.
+    """
+    _shared_state={}
+    def __new__(cls, *a, **k):
+        obj = object.__new__(cls, *a, **k)
+        obj.__dict__ = cls._shared_state
+        return obj
+
+class UserPreferences(Borg):
+    """Keeps track of user preferences such as:
+    language: the preferred language;
+    working_dir: the directory where user-created Python files are saved;
+    style: the preferred css style.
+    Eventually, more options (e.g. styling of Python programs) will be added.
+    """
+    __initialised = False
+    _language = None
+    def __init__(self, root_dir=None):
+        if not self.__initialised:
+            self.__initialised = True
+            self.root_dir = root_dir
+            translation.home = root_dir
+            self.styles = []
+            self.menu = None
+            self.config = SafeConfigParser()
+            self.changed = False
+            self.user_dir = os.path.join(os.path.expanduser("~"), ".crunchy")
+            if not os.path.exists(self.user_dir):  # first time ever
+                self.create_path()
+            self.user_file = os.path.join(self.user_dir, "crunchy.cfg")
+            self.load()
+        return
+
+    def create_path(self):
+        '''Creates a path for Crunchy when none exists.'''
+        try:
+            os.makedirs(self.user_dir)
+        except:
+            print "Could not create the user directory."
+            self.user_dir = os.getcwd()  # use crunchy's a a default.
+        return
+
+    def create_file(self):
+        '''Creates a configuration file when none exist.'''
+        self.config.add_section('preferences')
+        self.set_language('en')
+        self.set_working_dir(self.user_dir)
+        self.set_style('gradient1.css')
+        f = open(self.user_file, 'w')
+        self.config.write(f)
+        f.close()
+        return
+
+    def set_preference(self, option, value):
+        '''Set an individual preference option'''
+        self.config.set('preferences', option, value)
+        self.changed = True
+        return
+
+    def load(self):
+        '''Reads the configuration values and sets them for this session'''
+        try:
+            self.config.readfp(open(self.user_file, 'r'))
+        except:
+            self.create_file()
+        self.working_dir = self.config.get('preferences', 'working directory')
+        self.style = self.config.get('preferences', 'style')
+        self.language = self.config.get('preferences', 'language')
+        self.changed = False
+        return
+
+    def save(self):
+        '''If necessary, saves the changed values.'''
+        if self.changed:
+            f = open(self.user_file, 'w')
+            self.config.write(f)
+            f.close()
+            self.changed = False
+        return
+
+    #== the following are implemented as properties for transparent access
+
+    def get_working_dir(self):
+        return self._working_dir
+
+    def set_working_dir(self, path):
+        self._working_dir = path
+        self.set_preference('working directory', path)
+        return
+
+    def get_language(self):
+        return self._language
+
+    def set_language(self, lang):
+        if self._language is not None:
+            if self._language == lang:
+                return
+            else:
+                self.changed = True
+        self._language = lang
+        self.set_preference('language', lang)
+        # make sure we set it first in module translation as it has more
+        # choices for editarea
+        translation.select(self._language)
+        self._editarea_lang = translation.get_editarea_lang()
+
+        # more limited choices here.
+        if self._language == "fr":
+            self.home = "src/html/crunchy_index_fr.html"
+            self.options = "src/html/options_fr.html"
+        else: # English is the default
+            self.home = "src/html/crunchy_index.html"
+            self.options = "src/html/options.html"
+            self._language = "en"
+        self.extract_menu()
+        self.save()
+        return
+
+    def get_style(self):
+        return self._style
+
+    def set_style(self, style):
+        self._style = style
+        self.set_preference('style', style)
+        return
+
+    def get_editarea_language(self):
+        return self._editarea_lang
+
+    working_dir = property(get_working_dir, set_working_dir)
+    language = property(get_language, set_language)
+    style = property(get_style, set_style)
+    editarea_language = property(get_editarea_language)
+
+    def extract_menu(self):
+        filename = os.path.normpath(os.path.join(self.root_dir, self.home))
+        try:
+            tree = HTMLTreeBuilder.parse(filename)
+        except Exception, info:
+            print info
+        head = tree.find("head")
+        body = tree.find("body")
+        self.menu = body.find(".//div")
+        for link in head.findall('.//link'):
+            self.styles.append(link)
+        body.clear()
+        head.clear()
+        body.insert(0, self.menu)
+        for link in self.styles:
+            head.insert(0, link)
+        return
---
+++ branches/andre/merge/src/errors.py
@@ -0,0 +1,174 @@
+'''
+errors.py
+
+Handle errors produced while running Crunchy Frog, as well as Python
+tracebacks, displaying the result to the user in a friendly way.
+At present, this is just a very basic module.  These will eventually
+be translated to other languages.
+
+'''
+
+
+import sys
+import traceback
+from StringIO import StringIO
+
+from translation import _
+
+def get_traceback(code):
+    ex_type, ex_val, ex_trace = sys.exc_info()
+    ex_lineno = ex_trace.tb_next.tb_lineno
+    ex_line = code.splitlines(True)[ex_lineno - 1]
+    return _(u"Error on line %s:\n%s%s\n")%(ex_lineno, ex_line, ex_val)
+
+
+def get_syntax_error(code):
+    """
+    print out a syntax error
+    closely based on showsyntaxerror from the code module
+    in the standard library
+    """
+    filename = "crunchy_exec"
+    type, value, sys.last_traceback = sys.exc_info()
+    sys.last_type = type
+    sys.last_value = value
+    if filename and type is SyntaxError:
+        # Work hard to stuff the correct filename in the exception
+        try:
+            msg, (dummy_filename, lineno, offset, line) = value
+        except:
+            # Not the format we expect; leave it alone
+            pass
+        else:
+            # Stuff in the right filename
+            value = SyntaxError(msg, (filename, lineno, offset, line))
+            sys.last_value = value
+    list = traceback.format_exception_only(type, value)
+    retval = StringIO()
+    map(retval.write, list)
+    return retval.getvalue()
+
+# The basic javascript code used displays an alert window to the user and
+# goes back to the last page which did not have an error.
+#
+#This code could be "improved" by doing a custom "Crunchy Frog" alert, perhaps
+# using something like the code on http://slayeroffice.com/code/custom_alert/
+
+## Note: because css files often contain "%", we can't use them to build
+# a string that will then be formatted with " %s ..."%param
+# as the "%" in the css file will cause an error. This is why we
+# use a simple concatenation to build the error message string in
+# HTMLTreeBuilder_error_dialog and IOError_error_dialog.
+
+css = open('src/css/custom_alert.css').read()
+js = open('src/javascript/custom_alert.js').read()
+begin_javascript_alert = '''
+<html>
+<head>
+<title></title>
+<style>%s</style>
+<script type='text/javascript'>
+%s
+function normalAlert(){  // redefining it
+removeCustomAlert();
+history.go(-1);return false;}
+function init(){
+window.alert(\"'''%(css, js)
+
+end_javascript_alert='''\");
+}
+</script>
+</head>
+<body onload="init();">
+</body>
+</html>
+'''
+
+###------------------
+# Note:  The string passed to the javascript alert can not contain newlines.
+###------------------
+
+class CrunchyError(Exception):
+    '''General class to derive exceptions from.'''
+    pass
+
+class ColourNameError(CrunchyError):
+    '''Used for invalid colour specification in canvas'''
+    def __init__(self, colour):
+        self.colour = colour
+    def __str__(self):
+        return str(self.colour)
+
+class NoSoundError(CrunchyError):
+    '''Used for invalid trying to use sound when module is not present'''
+    def __init__(self, info):
+        self.msg = info
+    def __str__(self):
+        return str(self.msg)
+
+class PythonExecutionError(CrunchyError):
+    '''Used for handling general error from execution of user-code.'''
+    def __init__(self, info):
+        self.info = info
+    def __str__(self):
+        return str(self.info)
+
+class HTMLTreeBuilderError(CrunchyError):
+    '''Used for handling parsing errors by elementtree.'''
+    def __init__(self, path, info):
+        self.info = info
+        self.path = path.replace("\\", '/')
+    def __str__(self):
+        return _("Error in processing %s: %s") %(self.path, self.info)
+
+def HTMLTreeBuilder_error_dialog(e):
+    mesg = _("The file '%s' has some html errors in it: %s.")%(e.path,
+              e.info) + _("  It can not be displayed.")
+    return begin_javascript_alert + mesg + end_javascript_alert
+
+def IOError_error_dialog(info):
+    return begin_javascript_alert + str(info) + end_javascript_alert
+
+def colour_name_dialog(colour):
+    return _("Canvas error: %s is an invalid colour.")%colour
+
+def canvas_error_dialog(info):
+    return _("Canvas error.  Python info: %s.")%info
+
+def python_error_dialog(info):
+    '''General error dialog.  Sprinkle throughout.'''
+    return _("Python execution error.  Info: %s.")%info
+
+def parsing_error_dialog(info):
+    '''Information given when the code colourizer fails.'''
+    return _("Parsing error occurred in the following Python code.\nInfo: %s.")%info
+
+###================
+# The following are "unexpected" errors.  Typically, they are errors which
+# have never occurred, or not occurred since some bugs have been fixed,
+# or error of a different type than those that are expected to occur.
+# They are included as diagnostic tools "just in case".
+
+def unexpected_file_error_dialog(function, filename, info):
+    msg = _("Unexpected error in %s. ")%function +\
+            _("This occurred while trying to open %s. ")%filename +\
+            _("The information given by Python is: %s.")%info
+    return javascript_alert%msg
+
+def unexpected_KeyError_error_dialog(function, filename, TREES, info):
+    msg = _("Unexpected error in %s. ")%function +\
+        _("This occurred while trying to process the tree for %s. ")%filename +\
+        _("The files processed so far are %s. ")%TREES +\
+        _("The information given by Python is: %s.")%info
+    return javascript_alert%msg
+
+def unexpected_Tutorials_get_error_dialog(args):
+    msg=_("Unrecognized argument in Tutorials.get(): %s. ")%args +\
+        _("This should never happen - and has not happened since the last bug fix!")+\
+        _(" Please contact the developpers with a copy of the html file ")+\
+        _("that caused this problem, and a description of what was done.")
+    return javascript_alert%msg
+
+
+
+
