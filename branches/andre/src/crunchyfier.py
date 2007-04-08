5950da99a66750cbab1c0bfb22f70a12a112a6d8
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2007-04-08 15:09:48 +0000

    tool tip (using interpreter) now works on Mac OS.

--- branches/andre/src/crunchyfier.py
+++ branches/andre/src/crunchyfier.py
@@ -334,7 +334,7 @@ class VLAMPage(TreeBuilder):
         prompt.attrib['class'] = "stdin"
         prompt.text = ">>> "
         input = et.SubElement(elem, "input", type="text", id=id+"_input",
-               onkeyup='interp_trapkeys(event, "'+id+'","%s")'%_("Waiting..."))
+               onkeypress='interp_trapkeys(event, "'+id+'","%s")'%_("Waiting..."))
         input.attrib['class'] = "interp_input"
         interpreters.interps[id] = interpreters.HTTPrepl()
         tipbar = et.SubElement(self.body, "div", id=id+"_tipbar")
