81ca6290f46683e277b341a4e874f9e21a2a1b6e
johannes.wollard <johannes.wollard@aa0cd930-251a-0410-a94a-f18be9da9e77>
2007-04-27 23:35:37 +0000

    Tagging the 0.8.2 release

---
+++ tags/crunchy-0.8.2/src/css/styles.py
@@ -0,0 +1,5 @@
+# list of styles - excludes menu.css, default.css and custom_alert.css
+all_styles=[('gradient1.css', 'Colour gradients (earth tones)'),
+            ('b-w.css', 'Black and white'),
+            ('bold.css', 'Bold colours')
+            ]
