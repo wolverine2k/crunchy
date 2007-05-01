2660cdad20637b5a3d548a591e0749d33cd6f656
johannes.wollard <johannes.wollard@aa0cd930-251a-0410-a94a-f18be9da9e77>
2007-05-01 23:28:56 +0000

    Updating trunk. This was trivial using SVN merge.

---
+++ trunk/crunchy/plugins/links.py
@@ -0,0 +1,18 @@
+"""
+Rewrites links so that crunchy can access remote pages.
+"""
+
+import urllib
+
+import CrunchyPlugin as cp
+
+def register():
+    cp.register_vlam_handler("a", None, LinkHandler)
+    
+def LinkHandler(page, elem, uid, vlam):
+    """convert remote links if necessary"""
+    if "href" in elem.attrib:
+        href = elem.attrib["href"]
+        if href.startswith("http://"):
+            elem.attrib["href"] = "/remote?url=%s" % urllib.quote_plus(href)
+        
