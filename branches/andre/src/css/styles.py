41051fc76e9d48d15e0e1520c63d463f8be20824
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2007-05-03 23:12:49 +0000

    Removing andre's old branch.

--- branches/andre/src/css/styles.py
+++
@@ -1,5 +0,0 @@
-# list of styles - excludes menu.css, default.css and custom_alert.css
-all_styles=[('gradient1.css', 'Colour gradients (earth tones)'),
-            ('b-w.css', 'Black and white'),
-            ('bold.css', 'Bold colours')
-            ]
