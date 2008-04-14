7d0949c832932f14e0b8b3f962a9d227b7fc43b9
johannes.woolard <johannes.woolard@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-04-14 12:41:16 +0000

    Tagging a test release to test the my new release build system.

---
+++ src/plugins/vlam_load_local.py
@@ -0,0 +1,32 @@
+"""  Crunchy load local tutorial plugin.
+
+Creates a form allowing to browse for a local tutorial to be loaded
+by Crunchy.
+
+This module is meant to be used as an example of how to create a custom
+Crunchy plugin; it probably contains more comments than necessary
+for people familiar with the Crunchy plugin architecture.
+"""
+
+# All plugins should import the crunchy plugin API via interface.py
+from src.interface import plugin
+from src.utilities import insert_file_browser
+
+# The set of other "widgets/services" required from other plugins
+requires = set(["/local"])
+
+def register():
+    """The register() function is required for all plugins.
+       In this case, we need to register a single type of 'action':
+          a custom 'vlam handler' designed to tell Crunchy how to
+          interpret the special Crunchy markup.
+       """
+    # 'load_remote' only appears inside <span> elements, using the notation
+    # <span title='load_remote'>
+    plugin['register_tag_handler']("span", "title", "load_local",
+                                                 insert_load_local)
+
+def insert_load_local(dummy_page, parent, dummy_uid):
+    "Inserts a javascript browser object to load a local (html) file."
+    insert_file_browser(parent, 'Load local html tutorial', '/local')
+    return
