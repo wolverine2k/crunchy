9e9bd019e212dda19bee99babdd87a3d5021ae68
johannes.woolard <johannes.woolard@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-04-15 10:31:31 +0000

    Cleaning up, commit 2 of 3 (first went wrong)

--- crunchy/andre/src/imports/DOM/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-__init__.py
-
-Central organising file for the DOM
-"""
-
-from document import Document
--- crunchy/src/PluginServices.py
+++
@@ -1,1 +0,0 @@
-"""A dummy module used by plugin services, This is intentionally empty."""
