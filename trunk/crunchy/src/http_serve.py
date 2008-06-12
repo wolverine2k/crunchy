b2286a0a546484333f01535e5c595326c7b6c402
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-06-12 10:25:12 +0000

    Updating http_serve.py from Tao's branch after having used svn merge.

--- trunk/crunchy/src/http_serve.py
+++ trunk/crunchy/src/http_serve.py
@@ -9,13 +9,98 @@ treats them the same.
 
 from SocketServer import ThreadingMixIn, TCPServer
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import urllib
+import urllib,urllib2
 from traceback import format_exc
-import base64
+import base64,md5
+import time
 from src.interface import python_version, python_minor_version
+from src.utilities import uidgen
 import src.CrunchyPlugin as CrunchyPlugin
 
 DEBUG = False
+
+def require_basic_authenticate(func):
+    '''A decorate  to addd  basic http authorization check to HTTP Request Handlers'''
+    def wrapped(self):
+        #if not hasattr(self, 'need_authenticated'):
+        #    return func(self)
+        if not hasattr(self, 'authenticated'):
+            self.authenticated = False
+        if not self.authenticated and 'Authorization' in self.headers:
+            auth_word = self.headers['Authorization'].split()
+            if auth_word[0] != 'Basic':
+                self.authenticated = False 
+            elif len(auth_word) > 1:
+                user,password = base64.b64decode(auth_word[1]).split(':')
+                if user == 'crunchy' and password == 'crunchypassword':
+                    self.authenticated = True
+        if not self.authenticated:
+            self.send_response(401)
+            self.send_header('WWW-Authenticate','Basic realm="Crunchy Access"')
+            self.end_headers()
+            self.wfile.write("You are not allowed to access this page.") 
+    return wrapped
+
+def require_digest_access_authenticate(func):
+    '''A decorate  to addd  deigest authorization check to HTTP Request Handlers'''
+    #TODO:Find a bettter way to decide what method we are dealing with ..
+    if "GET" in func.__name__:
+        method = "GET"
+    else:
+        method = "POST"
+    realm = "Crunchy Access"          
+    users = {"crunchy" : "password"}
+
+    def wrapped(self):
+        #if not hasattr(self, 'need_authenticated'):
+        #    return func(self)
+        md5hex = lambda x:md5.md5(x).hexdigest()
+
+        if not hasattr(self, 'authenticated'):
+            self.authenticated =  None
+        auth = self.headers.getheader('authorization')
+        if not self.authenticated and auth is not None:
+            token, fields = auth.split(' ', 1)
+            if token == 'Digest':
+                cred = urllib2.parse_http_list(fields)
+                cred = urllib2.parse_keqv_list(cred)
+                if 'realm' not in cred or 'username' not in cred \
+                    or 'nonce' not in cred or 'uri' not in cred or 'response' not in cred:
+                    self.authenticated = False
+                elif cred['realm'] != realm or cred['username'] not in users:
+                    self.authenticated = False
+                elif 'qop' in cred and ('nc' not in cred or 'cnonce' not in cred): 
+                    self.authenticated = False
+                else:
+                    if 'qop' in cred:
+                        expect_response = md5hex('%s:%s'%( 
+                            md5hex('%s:%s:%s' %(cred['username'], realm , users.get(cred['username'], ""))),
+                            ':'.join([cred['nonce'],cred['nc'],cred['cnonce'],cred['qop'], md5hex('%s:%s' %(method, self.path))])
+                            )
+                        )
+                    else:
+                        expect_response = md5hex('%s:%s' %(
+                            md5hex('%s:%s:%s' %(cred['username'], realm , users.get(cred['username'], ""))),
+                            ':'.join([cred['nonce'], md5hex('%s:%s' %(method, self.path))])
+                            )
+                        )
+                    self.authenticated = (expect_response == cred['response'])
+
+        if self.authenticated is None:
+            msg = "You are not allowed to access this page.Please login first!"
+        elif self.authenticated is False:
+            msg = "Authenticated Failed"
+
+        if  not self.authenticated :
+            self.send_response(401)
+            self._nonce = md5hex("%d:%s" % (time.time(), realm))
+            self.send_header('WWW-Authenticate','Digest realm="%s", qop="auth", algorithm="MD5", nonce="%s"' %(realm, self._nonce))
+            self.end_headers()
+            self.wfile.write(msg) 
+        else:
+            return func(self)
+
+    return wrapped
 
 class MyHTTPServer(ThreadingMixIn, HTTPServer):
     daemon_threads = True
@@ -52,10 +137,11 @@ class MyHTTPServer(ThreadingMixIn, HTTPServer):
             return self.default_handler
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
+
+    #@require_basic_authenticate
+    @require_digest_access_authenticate
     def do_POST(self):
         """handle an HTTP request"""
-        if not self.check_authenticate():
-            return
         # at first, assume that the given path is the actual path and there are no arguments
         realpath = self.path
         if python_version >=3:
@@ -105,6 +191,8 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 ##            import sys
 ##            sys.exit()
 
+    #@require_basic_authenticate
+    @require_digest_access_authenticate
     def do_GET(self):
         """the same as POST, we draw no distinction"""
         self.do_POST()
@@ -113,20 +201,4 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         BaseHTTPRequestHandler.send_response(self, code)
         self.send_header("Connection", "close")
 
-    def check_authenticate(self):
-        if not hasattr(self, 'authenticated'):
-            self.authenticated = False
-        if not self.authenticated and 'Authorization' in self.headers:
-            auth_word = self.headers['Authorization'].split()
-            if auth_word[0] != 'Basic':
-                self.authenticated = False 
-            elif len(auth_word) > 1:
-                user,password = base64.b64decode(auth_word[1]).split(':')
-                if user == 'crunchy' and password == 'crunchypassword':
-                    self.authenticated = True
-        if not self.authenticated:
-            self.send_response(401)
-            self.send_header('WWW-Authenticate','Basic realm="Crunchy Access"')
-            self.end_headers()
-       
-        return self.authenticated
+
