7784f64dcea60fc3326037ebfbf0df5c676277c6
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-07-11 23:21:20 +0000

    Fixing a problem in main trunk: added two dummy functions to a new plugin (config_gui.py) so as to prevent Crunchy from aborting.

--- trunk/crunchy/src/plugins/config_gui.py
+++ trunk/crunchy/src/plugins/config_gui.py
@@ -14,15 +14,19 @@ def register():  # tested
     plugin['register_http_handler']("/config", config_page)
 
 
+def set_config():
+    pass
+def config_page():
+    pass
 
 ###############################################################
 #### We rely on a ConfigOption class to keep track of an option
-#### This is dummy code that will later be integrated into 
+#### This is dummy code that will later be integrated into
 #### configuration.py
 
 class ConfigOption(object):
     all_options = {}
-    
+
     def __init__(self, key, initial):
         """
         >>> obj = ConfigOption("test")
@@ -32,24 +36,24 @@ class ConfigOption(object):
         self.key = key
         self.set(initial)
         ConfigOption.all_options[key] = self
-        
+
     def get(self):
         return self.__value
-        
+
     def set(self, value):
         self.__value = value
-    
+
 class MultiOption(ConfigOption):
     """An option that has multiple predefined choices
     """
     def __init__(self, key, initial, values):
         super(MultiOption, self).__init__(key, initial)
         self.values = values
-        
+
     def get_values(self):
         """get the possible values"""
         return self.values
-        
+
 ################################################################
 
 class ConfigWidget(object):
@@ -57,27 +61,27 @@ class ConfigWidget(object):
 
     """
     next_id = 0
-    
+
     def __init__(self, option):
         self.option = option
         self.id = ConfigWidget.next_id
         ConfigWidget.next_id += 1
-    
+
     def pre_render(self, handle):
         handle.write("<div>")
-        
+
     def post_render(self, handle):
         handle.write("</div>")
-        
+
 class MultiChoiceWidget(ConfigWidget):
     """
     """
-    # if there are les than or equal to this number of values, use a 
+    # if there are les than or equal to this number of values, use a
     # radiobutton group, otherwise use a dropdown box
     threshold = 4
     def __init__(self, option):
         super(MultiChoiceWidget, self).__init__(option)
-        
+
     def render(self, handle):
         """render the widget to a particular file object"""
         values = self.option.get_values()
@@ -101,12 +105,11 @@ class MultiChoiceWidget(ConfigWidget):
                 if value != self.option.get():
                     handle.write('<option value="%s"> %s' % (value, value))
             handle.write("</select>")
-        
-    
+
+
 def _test():
     import doctest
     doctest.testmod()
 
 if __name__ == "__main__":
     _test()
-    
