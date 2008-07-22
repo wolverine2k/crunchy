094d47888d94cadac4ed5bee8a61a1d9c49418ad
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-07-22 04:35:56 +0000

    Started the integration of account_manager.py into andre's branch.  Note that this version of account_manager has many changes from Tao's version.  It is not finished yet - just checking in so as to make sure work is not lost.

---
+++ branches/andre/trunk/crunchy/account_manager.py
@@ -0,0 +1,240 @@
+'''Account Manager For Crunchy
+'''
+import md5
+import os
+import sys
+from getpass import getpass
+
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), ".PASSWD")
+
+class Accounts(dict): # tested
+    '''dict-derived class to store user's account information.'''
+
+    realm = 'Crunchy Access'
+    separator = '|'
+
+    def __init__(self, pwp=None): # tested
+        if pwp is None:  # use this to allow redefining DEFAULT_PATH
+            pwp = DEFAULT_PATH # from outside this module
+        self.pwd_file_path = pwp
+        self.base_dir = os.path.join(os.path.expanduser("~"), ".crunchy")
+        if os.path.exists(pwp):
+            try:
+                self.load()
+            except IOError:
+                print "WARNING: Could not open existing password file."
+        else:
+            print "New password file [path = %s] will be created." % pwp
+
+    def __setitem__(self, username, item): # tested indirectly
+        '''overrides base class dict method so that the password gets
+        automatically encrypted.'''
+        home = item[0]
+        encoded_password = md5.md5('%s:%s:%s' %
+                           (username, self.realm, item[1])).hexdigest()
+        dict.__setitem__(self, username, (home, encoded_password))
+
+    def load(self): # tested indirectly
+        '''loads data from password file'''
+        data = open(self.pwd_file_path, 'rb').read()
+        for line in data.split('\n'):
+            if not line:
+                continue
+            username, user_dir, encoded_password = line.split(self.separator, 2)
+            dict.__setitem__(self, username, (user_dir, encoded_password))
+
+    def save(self): # tested
+        '''saves the account information in a file.'''
+        lines = []
+        for key, value in self.items():
+            lines.append(self.separator.join([key, value[0], value[1]]))
+        data = '\n'.join(lines)
+        f = open(self.pwd_file_path, 'w')
+        f.write(data)
+        f.close()
+
+    def get_password(self, username): # tested
+        '''given a username, return the corresponding password, if it is a
+           known username, otherwise returns an empty string.'''
+        if username not in self:
+            return ""
+        else:
+            return self[username][1]
+
+    def get_home_dir(self, username): # tested
+        '''given a username, return the corresponding home directory
+           if it is a known username, otherwise returns an empty string.'''
+        if username not in self:
+            return ""
+        else:
+            return self[username][0]
+
+class AMCLI(object):
+    '''Account Manager Command Line Interface'''
+
+    def __init__(self, pwp=None):
+        if pwp is None:  # use this to allow redefining DEFAULT_PATH
+            pwp = DEFAULT_PATH # from outside this module
+        self.welcome_msg = "Crunchy's Account Manager.\n" +\
+                           "For information, type help.\n"
+        self.accounts = Accounts(pwp)
+        self.interact()
+
+    def interact(self):
+        '''starts the interactive mode (command line interface)'''
+        print self.welcome_msg
+        while True:
+            command = ""
+            try:
+                command = raw_input(">>> ")
+            except EOFError,e:
+                print "Please use 'exit' to exit the account manager."
+                continue
+            if command:
+                self.on_command(command)
+
+    def on_command(self, command):
+        'command dispatcher'
+        command = command.strip().split(" ", 1)
+        if hasattr(self, "cmd_" + command[0]):
+            if command[0] in ('list', 'exit', 'help', 'set_base_dir'):
+                getattr(self, "cmd_" + command[0])()
+            elif len(command) > 1:
+                getattr(self, "cmd_" + command[0])(command[1])
+            else:
+                print ("Argument username is needed for command %s." %
+                                                                (command[0]))
+        else:
+            print "Unknown command."
+
+    def cmd_set_base_dir(self):
+        '''set_base_dir : sets the base directory from which accounts are
+           created when choosing 'default'.'''
+        print "The current base directory is %s" % accounts.base_dir
+        print "If you want to keep the same base directory, simply press 'enter'."
+        base_dir = raw_input("Please enter the new base directory: ")
+        if base_dir != "":
+            accounts.base_dir = base_dir
+            print "The account information for 'username' will be saved in "
+            print os.path.join(accounts.base_dir, 'username')
+        return
+
+    def cmd_exit(self):
+        'exit : exit the program (accounts are saved automatically)'
+        raise SystemExit
+
+    def cmd_list(self):
+        'list : list all users'
+        print "%s\t%s" % ("username", "home directory")
+        print '---------------------------------------'
+        for user in self.accounts:
+            print ("%s\t\t%s" %(user, self.accounts[user][0]))
+
+    def cmd_new(self, username):
+        'new <username> : add a new user.'
+        home = raw_input("Please input the home directory [or 'default']: ")
+        while True:
+            password = getpass("Please input the password: ")
+            password2 = getpass("Please input the password again: ")
+            if password == password2:
+                break
+            else:
+                print "The passwords do not match; please try again."
+        if username in self.accounts:
+            print "Error: user %s already exists" % (username)
+        else:
+            home = self.evaluate_home(username, home)
+            self.accounts[username] = (home, password)
+            self.accounts.save()
+
+    def cmd_edit(self, username):
+        "edit <username> : change a user's home directory and password."
+        if username not in self.accounts:
+            print "Error: user %s doesn't exist" % (username)
+        else:
+            home = raw_input("Please input the new home directory:[%s]" %
+                                                  (self.accounts[username][0]))
+            if not home:
+                home = self.accounts[username][0]
+            else:
+                home = self.evaluate_home(username, home)
+            while True:
+                password = getpass("Please input the new password:")
+                password2 = getpass("Please input the password again:")
+                if password == password2:
+                    break
+                else:
+                    print "Password don't match, please try again."
+            self.accounts[username] =  (home, password)
+            self.accounts.save()
+
+    def cmd_del(self, username):
+        "del <username> : delete a user"
+        if username not in self.accounts:
+            print "Error: user %s doesn't exist" % username
+        else:
+            del self.accounts[username]
+            self.accounts.save()
+
+    def cmd_load(self, file_name):
+        '''load <file_name> : create accounts from plain file <file_name>
+                File format :
+                    username_1 home_directory1 password1
+                    username_2 home_directory2 password2
+                    ...
+                You can use a single space [' '], a comma [','], or
+                a tab character ['\\t'] as separator between values.'''
+        f = open(file_name)
+        lines = f.readlines()
+        try_sep = ('\t', ' ', ',')
+        for sep in try_sep:
+            data = []
+            is_ok = True
+            for line in lines:
+                t = line.split(sep)
+                if len(t) != 3:
+                    is_ok = False
+                    break
+                else:
+                    data.append(t)
+            if is_ok:
+                break
+        else:
+            print "Error: Can't parse input file %s" % file_name
+        for t in data:
+            self.accounts[t[0]] = t[1:]
+        self.accounts.save()
+        print "%d accounts loaded." % len(data)
+
+    def cmd_help(self, topic=None):
+        "help [<help_topic>]: display this message or display help of <help_topic>"
+        self.help(topic)
+
+    def help(self, topic=None):
+        'display the requested help information.'
+        if topic is None:
+            msg = "Crunchy's Account Manager supported commands:\n"
+            for attr in dir(self):
+                if attr.startswith('cmd_'):
+                    msg += "  %s\n" % (getattr(self, attr).__doc__)
+            print msg
+        else:
+            if hasattr(self, 'cmd_' + topic):
+                print getattr(self, 'cmd_' + topic).__doc__ + '\n'
+            else:
+                print "No such command %s" % topic
+
+    def evaluate_home(self, username, home):
+        '''processes home directory as provided by user to replace
+        "shortcuts" by their true values.'''
+        if home == "default":
+            return os.path.join(accounts.base_dir, username)
+        else:
+            return home
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        pw_file_path = sys.argv[1]
+        am = AMCLI(pw_file_path)
+    else:
+        am = AMCLI()
---
+++ branches/andre/trunk/crunchy/src/tests/test_no_markup.rst
@@ -0,0 +1,62 @@
+no_markup.py tests
+================================
+
+no_markup.py has has the following functions that require testing:
+
+1. `register()`_
+#. `custom_vlam()`_
+
+0. Setting things up
+--------------------
+
+    >>> from src.interface import plugin, config, Element, tostring
+    >>> plugin.clear()
+    >>> config.clear()
+    >>> import src.plugins.no_markup as no_markup
+    >>> import src.tests.mocks as mocks
+    >>> mocks.init()
+    >>> def repeat_args(*args):
+    ...      for arg in args:
+    ...          print arg
+    >>>
+
+
+.. _`register()`:
+
+Testing register()
+----------------------
+
+    >>> no_markup.register()
+    >>> print mocks.registered_final_tag_handlers #doctest: +ELLIPSIS
+    {'pre': <function custom_vlam at ...>}
+
+
+.. _`custom_vlam()`:
+
+Testing custom_vlam()
+--------------------------
+
+First, no markup specified.
+
+    >>> page = mocks.Page()
+    >>> page.pre = Element("pre")
+    >>> config['no_markup'] = None
+    >>> no_markup.custom_vlam(page, page.pre, '42')
+    >>> print tostring(page.pre).replace('>', '>\n')
+    <pre />
+    <BLANKLINE>
+
+Next, some silly markup...
+
+    >>> config['no_markup'] = "silly"
+    >>> page.handlers3["pre"] = {}
+    >>> page.handlers3["pre"]["title"] = {}
+    >>> page.handlers3["pre"]["title"]["silly"] = repeat_args # fake handler
+    >>> no_markup.custom_vlam(page, page.pre, '42') #doctest: +ELLIPSIS
+    <src.tests.mocks.Page object at ...>
+    <Element pre at ...>
+    42
+    >>> print tostring(page.pre).replace('>', '>\n')
+    <pre title="silly" />
+    <BLANKLINE>
+
