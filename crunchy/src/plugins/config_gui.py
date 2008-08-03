61400c6b9a159ddb5e3f4551bdfa32083938cfae
florian.biree <florian.biree@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-08-03 17:17:12 +0000

    Dynamic configuration page: form rendering (currently no option can be set). See the /config page.

--- crunchy/src/plugins/config_gui.py
+++ crunchy/src/plugins/config_gui.py
@@ -3,42 +3,60 @@ config_gui.py : a plugin to enable users to configure crunchy nicely.
 
 """
 
-from src.interface import plugin
+from src.interface import translate, plugin, config
+from src.configuration import options, ANY
+_ = translate['_']
 
 provides = set(["/comet", "/input"])
 
 def register():  # tested
-    '''registers two http handlers: /config and /set_config'''
+    '''registers two http handlers: /config and /set_config and a begin page
+    handler to insert the configuration page'''
     plugin['register_http_handler'](
                     "/set_config%s" % plugin['session_random_id'], set_config)
     plugin['register_http_handler']("/config", config_page)
-    # TODO create autogeneration once new config framework is in place
 
 # the following are required for Crunchy to work; they will need to be defined.
 def set_config(request):
+    """Http handler to set an option"""
     option = ConfigOption.all_options[request.args['option']]
     option.set(request.args['value'])
     
 def config_page(request):
+    """Http handler to make a dynamic configuration page"""
+    # Dynamic generation of the option list
+    for key in options:
+        if key in config:
+            if set(options[key]) == set((True, False)):
+                # boolean option
+                BoolOption(key, config[key])
+            elif ANY in options[key]:
+                # any string allowed
+                StringOption(key, config[key])
+            else:
+                # multiple predefined choices
+                MultiOption(key, config[key], options[key])
+    # Rendering the page
     request.wfile.write(config_head_html)
-    for option in ConfigOption.all_options:
-        option.render(request.wfile)
+    for key in ConfigOption.all_options:
+        ConfigOption.all_options[key].render(request.wfile)
     request.wfile.write(config_tail_html)
 
 class ConfigOption(object):     # tested
+    """Generic option class"""
     all_options = {}
 
     def __init__(self, key, initial):       # tested
-        """
-        """
         self.key = key
         self.set(initial)
         ConfigOption.all_options[key] = self
 
     def get(self):      # tested
+        """Return the current value of the option"""
         return self.__value
 
     def set(self, value):   # tested
+        """Define the value of the option"""
         self.__value = value
 
 class MultiOption(ConfigOption):        # tested
@@ -58,28 +76,84 @@ class MultiOption(ConfigOption):        # tested
     def render(self, handle):
         """render the widget to a particular file object"""
         values = self.get_values()
-        handle.write("<div>\n")
+        handle.write('<p>\n')
         if len(values) <= MultiOption.threshold:
+            handle.write('%(name)s:<br />\n' % {'name': self.key})
             for value in values:
-                handle.write('<input type="radio" name="')
-                handle.write(self.key)
-                handle.write('" value="')
-                handle.write(value)
-                handle.write('" ')
-                if value == self.get():
-                    handle.write('checked="checked" ')
-                handle.write(" />")
-                handle.write(value + "<br />\n")
+                handle.write('''
+                <input type="radio" name="%(key)s" id="%(key)s-%(value)s"
+                value="%(value)s" %(checked)s />
+                <label for="%(key)s-%(value)s">%(value)s</label>
+                <br />
+                ''' % {
+                    'key': self.key,
+                    'value': str(value), # str( ) is needed for None
+                    'checked': 'checked="checked"' if value == self.get() \
+                                                   else '',
+                })
                 # TODO insert some javasrcipt
         else:
-            handle.write('<select name="%s">' % self.key)
-            # current value first
-            handle.write('<option value="%s"> %s' % (self.get(), self.get()))
+            handle.write('''
+            <label for="%(key)s">%(name)s</label>:
+            <select name="%(key)s" id="%(key)s">
+            ''' % {
+                'key': self.key,
+                'name': self.key,
+            })
             for value in values:
-                if value != self.get():
-                    handle.write('<option value="%s"> %s' % (value, value))
+                handle.write('''
+                <option value="%(value)s" %(selected)s>%(value)s</option>
+                ''' % {
+                    'value': str(value), # str( ) is needed for None
+                    'selected': 'selected="selected"' if value == self.get() \
+                                                      else '',
+                }) 
+                # TODO insert some javasrcipt
             handle.write("</select>")
-        handle.write("</div>")
+        handle.write("</p>")
 
-config_head_html = """<html><head><title>Crunchy :: Config</title></head><body>"""
+class BoolOption(ConfigOption):
+    """An option that has two choices [True, False]
+    """
+    
+    def render(self, handle):
+        """render the widget to a particular file object"""
+        handle.write('''
+        <p>
+            <input type="checkbox" name="%(key)s" id="%(key)s" %(checked)s />
+            <label for="%(key)s">%(name)s</label>
+        </p>
+        ''' % {
+            'key': self.key,
+            'name': self.key,
+            'checked': 'checked="checked"' if self.get() else '',
+        })
+        # TODO insert some javasrcipt
+
+class StringOption(ConfigOption):
+    """An option that can have any value
+    """
+    
+    def render(self, handle):
+        """render the widget to a particular file object"""
+        handle.write('''
+        <p>
+            <label for="%(key)s">%(name)s</label>:
+            <input type="text" id="%(key)s" name="%(key)s" value="%(value)s" />
+        </p>
+        ''' % {
+            'key': self.key,
+            'name': self.key,
+            'value': self.get(),
+        })
+
+config_head_html = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+    <title>Crunchy :: Config</title>
+</head>
+<body>
+"""
 config_tail_html = """</body></html>"""
