58b31bb4dbe5fed19d2d3fcec9feadc4821147ca
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-08-30 01:07:04 +0000

    Created a tool to help design styles (css classes) for Pygments.

--- branches/andre/trunk/crunchy/src/plugins/style.py
+++ branches/andre/trunk/crunchy/src/plugins/style.py
@@ -5,6 +5,7 @@ from pygments.lexers import get_lexer_by_name, guess_lexer
 from pygments.formatters import HtmlFormatter
 from pygments.styles import get_style_by_name, get_all_styles
 from pygments.lexers._mapping import LEXERS
+from pygments.token import STANDARD_TYPES
 
 from src.interface import fromstring, plugin, SubElement, additional_properties, config
 from src.configuration import make_property, options
@@ -18,6 +19,12 @@ for name in LEXERS:
         _pygment_language_names.append(alias)
 
 lexers = {}
+options['style'] = list(get_all_styles())
+additional_properties['style'] = make_property('style', default='crunchy',
+doc="""\
+Style used by pygments to colorize the code.  In addition to the default
+value ['crunchy'], it includes all the styles [css classes] included
+in the pygments distribution.""")
 
 def register():
     for language in _pygment_language_names:
@@ -26,9 +33,8 @@ def register():
     for language in _pygment_lexer_names:
         plugin["register_tag_handler"]("code", "title", language, pygment_style)
         plugin["register_tag_handler"]("pre", "title", language, pygment_style)
-    options['cssclass'] = list(get_all_styles())
-    additional_properties['cssclass'] = make_property('cssclass', default='crunchy',
-                                                      doc="some help")
+    plugin["register_tag_handler"]("div", "title", "get_pygments_tokens",
+                                   get_pygments_tokens)
 
 def extract_code(elem):
     text = elem.text or ""
@@ -41,8 +47,8 @@ def extract_code(elem):
 def pygment_style(page, elem, dummy_uid):
     language = elem.attrib['title']
     text = extract_code(elem)
-    cssclass = config[page.username]['cssclass']
-    styled_code = style(text, language, cssclass)
+    cssclass = config[page.username]['style']
+    styled_code = _style(text, language, cssclass)
     markup = fromstring(styled_code)
     elem.text = ''
     elem[:] = markup[:]
@@ -50,6 +56,38 @@ def pygment_style(page, elem, dummy_uid):
     if not page.includes("pygment_cssclass"):
         page.add_css_code(HtmlFormatter(style=cssclass).get_style_defs("."+cssclass))
         page.add_include("pygment_cssclass")
+    return
+
+def get_pygments_tokens(page, elem, uid):
+    """inserts a table containing all existent token types and corresponding
+       css class, with an example"""
+    # The original div in the raw html page may contain some text
+    # as a visual reminder that we need to remove here.
+    elem.text = ''
+    elem.attrib['class'] = config[page.username]['style']
+    table = SubElement(elem, 'table')
+    row = SubElement(table, 'tr')
+    for title in ['Token type', 'css class']:
+        column = SubElement(row, 'th')
+        column.text = title
+    keys = STANDARD_TYPES.keys()
+    keys.sort()
+    for token in keys:
+        if len(repr(token)) == 5: # token = Token
+            continue
+        row = SubElement(table, 'tr')
+        column1 = SubElement(row, 'td')
+        column1.text = repr(token)[6:] # remove "Token."
+        column2 = SubElement(row, 'td')
+        column2.text = STANDARD_TYPES[token]
+        column3 = SubElement(row, 'td')
+        _code = SubElement(column3, 'code')
+        _code.attrib['class'] = column2.text
+        _code.text = " * test * "
+        column4 = SubElement(row, 'td')
+        var = SubElement(column4, 'var')
+        var.attrib['class'] = column2.text
+        var.text = " * test * "
     return
 
 class PreHtmlFormatter(HtmlFormatter):
@@ -66,7 +104,7 @@ class PreHtmlFormatter(HtmlFormatter):
         yield 0, '</pre>'
 
 
-def style(raw_code, language, cssclass="highlight"):
+def _style(raw_code, language, cssclass="crunchy"):
     """Returns a string of formatted and styled HTML, where
     raw_code is a string, language is a string that Pygments has a lexer for,
     and cssclass is a class style available for Pygments."""
