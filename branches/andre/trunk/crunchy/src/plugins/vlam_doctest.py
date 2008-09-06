72bf9e53233487bdef282fc54f8c3381b7d7038f
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-09-06 14:21:30 +0000

    analyzer options working in doctest.

--- branches/andre/trunk/crunchy/src/plugins/vlam_doctest.py
+++ branches/andre/trunk/crunchy/src/plugins/vlam_doctest.py
@@ -99,6 +99,10 @@ def doctest_widget_callback(page, elem, uid):
     btn = SubElement(elem, "button")
     btn.text = "Run Doctest"
     btn.attrib["onclick"] = "exec_doctest('%s')" % uid
+    if "analyzer_score" in vlam:
+        plugin['services'].add_scoring(page, btn, uid)
+    if "analyzer_report" in vlam:
+        plugin['services'].insert_analyzer_button(page, elem, uid)
     SubElement(elem, "br")
     # finally, an output subwidget:
     plugin['services'].insert_io_subwidget(page, elem, uid)
