606e9115408b941adeecbbcd83e0a099a5135558
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2008-10-02 21:11:51 +0000

    Saving file from editarea re-implemented.

--- crunchy/src/plugins/editarea.py
+++ crunchy/src/plugins/editarea.py
@@ -42,15 +42,15 @@ def add_hidden_load_and_save(page, elem, textarea_id):  # tested
     hidden_load_id = 'hidden_load' + textarea_id
     hidden_load = SubElement(elem, 'div', id=hidden_load_id)
     hidden_load.attrib['class'] = 'load_python'
-    addLoadPython(page, hidden_load, hidden_load_id, textarea_id)
+    add_load_python(page, hidden_load, hidden_load_id, textarea_id)
 
     hidden_save_id = 'hidden_save' + textarea_id
     hidden_save = SubElement(elem, 'div', id=hidden_save_id)
     hidden_save.attrib['class'] = 'save_python'
-    addSavePython(hidden_save, hidden_save_id, textarea_id)
+    add_save_python(page, hidden_save, hidden_save_id, textarea_id)
     return
 
-def insert_file_browser(page, elem, uid, action, title, js_script):
+def insert_file_browser(page, elem, uid, action, title, js_script, klass):
     '''inserts a file tree object in a page.'''
     if 'display' not in config[page.username]['page_security_level'](page.url):
         if not page.includes("jquery_file_tree"):
@@ -75,21 +75,21 @@ def insert_file_browser(page, elem, uid, action, title, js_script):
     """ % (tree_id, root, action, js_script)
     page.add_js_code(js_code)
     elem.text = title
-    elem.attrib['class'] = "load_python"
+    elem.attrib['class'] = klass
 
     file_div = SubElement(elem, 'div')
     file_div.attrib['id'] = tree_id
     file_div.attrib['class'] = "filetree_window"
     return
 
-def addLoadPython(page, parent, hidden_load_id, textarea_id):
+def add_load_python(page, parent, hidden_load_id, textarea_id):
     '''Inserts the widget required to browse for and load a local Python
        file.  This is intended to be used to load a file in the editor.
     '''
     js_script = """path = file;
                    load_python_file('%s')""" % textarea_id
     insert_file_browser(page, parent, hidden_load_id, '/jquery_file_tree_all',
-                 _('Select a file'), js_script)
+                 _('Select a file to open'), js_script, "load_python")
     btn = SubElement(parent, 'button',
         onclick="c=getElementById('%s');c.style.visibility='hidden';c.style.zIndex=-1;"%hidden_load_id)
     btn.text = _("Cancel")
@@ -113,41 +113,53 @@ def jquery_file_tree_all(request):
     plugin['services'].filtered_dir(request, filter_none)
     return
 
-#def editarea_loader(request):
-#    """Loads source file from disk.
-#       """
-#    print request.args
-#    url = request.args["url"]
-#    # we may want to use urlopen for this?
-#    source_code = open(url).read()
+#def add_save_python(parent, hidden_save_id, textarea_id):  # tested
+#    '''Inserts the two forms required to browse for and load a local Python
+#       file.  This is intended to be used to load a file in the editor.
+#    '''
+#    filename = 'filename' + hidden_save_id
+#    path = 'path' + hidden_save_id
+#    SubElement(parent, 'br')
+#    form1 = SubElement(parent, 'form')
+#    SubElement(form1, 'input', type='file', id=filename, size='80')
+#    SubElement(form1, 'br')
+#
+#    form2 = SubElement(parent, 'form')
+#    form2.text = _("Use 'Save and Run' to execute programs (like pygame and GUI based ones) externally.")
+#    SubElement(form2, 'input', type='hidden', id=path)
+#    btn = SubElement(parent, 'button',
+#        onclick="a=getElementById('%s');b=getElementById('%s');a.value=b.value;"%(path, filename)+
+#        "c=getElementById('%s');path=c.value;save_python_file(path,'%s');"%(path, textarea_id))
+#    btn.text = _("Save Python file")
+#    btn2 = SubElement(parent, 'button',
+#        onclick="c=getElementById('%s');path=c.style.visibility='hidden';c.style.zIndex=-1;"%hidden_save_id)
+#    btn2.text = _("Cancel")
+#    btn3 = SubElement(parent, 'button',
+#        onclick="a=getElementById('%s');b=getElementById('%s');a.value=b.value;"%(path, filename)+
+#        "c=getElementById('%s');path=c.value;save_and_run(path,'%s');"%(path, textarea_id))
+#    btn3.text = _("Save and Run")
+#    return
 
-def addSavePython(parent, hidden_save_id, textarea_id):  # tested
-    '''Inserts the two forms required to browse for and load a local Python
+def add_save_python(page, parent, hidden_save_id, textarea_id):
+    '''Inserts the widget required to browse for and save a local Python
        file.  This is intended to be used to load a file in the editor.
     '''
-    filename = 'filename' + hidden_save_id
-    path = 'path' + hidden_save_id
-    SubElement(parent, 'br')
-    form1 = SubElement(parent, 'form')
-    SubElement(form1, 'input', type='file', id=filename, size='80')
-    SubElement(form1, 'br')
+    input_id = "input_" + hidden_save_id
+    js_saved_script = """save_python_file(document.getElementById('%s').value,
+                        '%s');""" % (input_id, textarea_id)
+    SubElement(parent, "br")
+    SubElement(parent, 'input', name='url', size='60', id=input_id)
+    SubElement(parent, "br")
+    btn = SubElement(parent, 'button', onclick=js_saved_script)
+    btn.text = _("Save file")
 
-    form2 = SubElement(parent, 'form')
-    form2.text = _("Use 'Save and Run' to execute programs (like pygame and GUI based ones) externally.")
-    SubElement(form2, 'input', type='hidden', id=path)
+    js_script = """document.getElementById('%s').value=file;""" % input_id
+    insert_file_browser(page, parent, hidden_save_id, '/jquery_file_tree_all',
+                 _('Select a file to save'), js_script, "save_python")
     btn = SubElement(parent, 'button',
-        onclick="a=getElementById('%s');b=getElementById('%s');a.value=b.value;"%(path, filename)+
-        "c=getElementById('%s');path=c.value;save_python_file(path,'%s');"%(path, textarea_id))
-    btn.text = _("Save Python file")
-    btn2 = SubElement(parent, 'button',
-        onclick="c=getElementById('%s');path=c.style.visibility='hidden';c.style.zIndex=-1;"%hidden_save_id)
-    btn2.text = _("Cancel")
-    btn3 = SubElement(parent, 'button',
-        onclick="a=getElementById('%s');b=getElementById('%s');a.value=b.value;"%(path, filename)+
-        "c=getElementById('%s');path=c.value;save_and_run(path,'%s');"%(path, textarea_id))
-    btn3.text = _("Save and Run")
+        onclick="c=getElementById('%s');c.style.visibility='hidden';c.style.zIndex=-1;"%hidden_save_id)
+    btn.text = _("Cancel")
     return
-
 
 editArea_load_and_save = """
 function my_load_file(id){
@@ -204,8 +216,21 @@ function load_python_file(obj_id)
 }
 function save_python_file(path, id)
 {
+    if (path == ''){
+        alert("No file specified");
+        var obj = document.getElementById('hidden_save'+id);
+        obj.style.visibility = "hidden";
+        obj.style.zIndex = -1;
+        return;
+    }
+    else{
+        alert(path);
+    }
     data = document.getElementById(id).value;
-    document.getElementById("path_"+id.substring(5)).innerHTML = path;
+    try{
+        document.getElementById("path_"+id.substring(5)).innerHTML = path;
+        }
+    catch (e){};
     var j = new XMLHttpRequest();
 	j.open("POST", "/save_file", true);
 	// Use an unlikely part of a filename (path) as a separator between file
@@ -253,11 +278,12 @@ load_save_css = """
     the screen so that they can be seen even when the editor is
     in fullscreen mode.  z-index of editarea when toggled is 9999 */
 
-.load_python{position:fixed; top:100px; z-index:-1;
-            border:4px solid #339; border-style: outset;
-            visibility:hidden; background-color:#66C;
-            color: white; font: 16pt; padding:15px;}
-.save_python{position:fixed; top:200px; z-index:-1;
-            border:4px solid #063; border-style: outset;
-            visibility:hidden; background-color:#696}
+.load_python{position: fixed; top: 100px; z-index:-1;
+            border: 4px solid #339; border-style: outset;
+            visibility: hidden; background-color: #66C;
+            color: white; font: 16pt; padding: 15px;}
+.save_python{position: fixed; top: 200px; z-index:-1;
+            border: 4px solid #063; border-style: outset;
+            visibility: hidden; background-color:#696;
+            color: white; font: 16pt; padding: 15px;}
 """
