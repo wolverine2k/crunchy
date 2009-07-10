2bac1390243efa7ed073d6addceb5ffc6162bece
shadytrees <shadytrees@aa0cd930-251a-0410-a94a-f18be9da9e77>
2009-07-10 21:55:01 +0000

    GSOC 2009: Unicode problems fixed in power_browser with tests added.

--- branches/hlian/gsoc_2009/crunchy/src/plugins/power_browser.py
+++ branches/hlian/gsoc_2009/crunchy/src/plugins/power_browser.py
@@ -10,34 +10,39 @@ from src.interface import plugin, config, Element, SubElement, translate, \
 _ = translate['_']
 
 def register(): # tested
-    """The register() function is required for all plugins.
-       """
+    """The register() function is required for all plugins."""
+
     plugin['register_end_pagehandler'](insert_browser)
     plugin['register_begin_pagehandler'](add_browsing_to_menu)
 
 def add_browsing_to_menu(dummy):
-    '''adds a menu item allowing the user to go the the browsers page'''
+    '''Adds a menu item allowing the user to go the the browsers
+    page.'''
+
     menu_item = Element("li")
-    link = SubElement(menu_item, 'a', href="/docs/basic_tutorial/browsing.html")
-    link.text = _("Browsing")
+    link = SubElement(menu_item, 'a',
+                      href=u"/docs/basic_tutorial/browsing.html")
+    link.text = _(u"Browsing")
     additional_menu_items['browsing'] = menu_item
 
 def insert_browser(page, *dummy): # tested
     '''Inserts a default file/url browser at the top of a page'''
+
     div = Element("div")
-    div.text = ' '
+    div.text = u' '
 
     if config[page.username]['power_browser'] is None:
         return
-    else:
-        try:
-            plugin[config[page.username]['power_browser']](page, div, 'dummy')
-            page.body.insert(0, div)
-        except: # unrecognized value, just ignore... (see below)
-            return
 
-# Why ignoring the exception?  Perhaps the browser requested is not available.
-# For example, suppose the user upgrades to a new Python version but do not
-# install docutils. As a result, she will not be able to process rst files.
-# However, she could have set the configuration to include an rst browser
-# while using a previous version of Python ...
+    try:
+        plugin[config[page.username]['power_browser']](page, div, 'dummy')
+        page.body.insert(0, div)
+    except Exception: # unrecognized value, see below
+        return
+
+# Why ignore the exception? Perhaps the browser requested is not
+# available. For example, suppose the user upgrades to a new Python
+# version but does not install docutils. As a result, she will not be
+# able to process the RST files. However, she could have set the
+# configuration to include an RST browser while using a previous
+# version of Python.
