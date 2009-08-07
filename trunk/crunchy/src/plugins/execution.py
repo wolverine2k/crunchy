476cd9070b0e5bf5d6d42bd5aa1b181c541afcc8
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2009-08-07 21:02:56 +0000

    Basic editor example now works properly with Python 3.

--- trunk/crunchy/src/plugins/execution.py
+++ trunk/crunchy/src/plugins/execution.py
@@ -1,7 +1,7 @@
 """This plugin handles all execution requests on /exec, ie. the standard execution method."""
 
 # All plugins should import the crunchy plugin API via interface.py
-from src.interface import plugin
+from src.interface import plugin, python_version
 
 # provides and require use the bare name ...
 provides = set(["/exec"])
@@ -13,6 +13,8 @@ def register():  # tested
 
 def exec_handler(request):  # tested
     """handle an execution request"""
+    if python_version > 2:
+        request.data = request.data.decode('utf-8')
     plugin['exec_code'](request.data, request.args["uid"])
     request.send_response(200)
     request.end_headers()
