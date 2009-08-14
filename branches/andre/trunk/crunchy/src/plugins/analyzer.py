cd0df501553852cf6a0bf206ef9b9e7e46ed5044
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2009-08-14 21:24:43 +0000

    Improvements to pychecker plugin.

--- branches/andre/trunk/crunchy/src/plugins/analyzer.py
+++ branches/andre/trunk/crunchy/src/plugins/analyzer.py
@@ -111,8 +111,9 @@ def analyzer_score_callback(request):
     plugin['append_text'](pageid, uid, "\n")
     score = analyzer.get_global_score()
     if score is not None:
-        plugin['append_text'](pageid, uid, _("[Code quality: %.2f/10]\n") % \
-                              score)
+        plugin['append_text'](pageid, uid,
+                    _("[From %s, code quality: %.2f/10]\n") %
+                        (config[request.crunchy_username]['analyzer'], score))
 
 def analyzer_widget_callback(page, elem, uid):
     """Handles embedding suitable code into the page in order to display and
