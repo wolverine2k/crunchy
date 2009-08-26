795d7d221d6d4df8f80094e3e3beca740ced803d
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2009-08-26 01:27:22 +0000

    Started work on "Crunchy rst2s5"

---
+++ trunk/crunchy/crst2s5.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: André Roberge
+# Copyright: This module has been placed in the public domain.
+
+"""
+crst2s5.py (or: Crunchy rst2s5.py) is an extension of rst2s5.py, the
+minimal front end to the Docutils Publisher, producing HTML slides using
+the S5 template system.
+
+It uses an updated version (1.2a2) of slides.js by Eric Meyer, which allows
+speaker notes to appear in separate window.
+
+Special Crunchy directives are also available.
+"""
+# There were three possible approaches to create crst2s5:
+# 1. Fork the docutils distribution, make the small changes required to
+#    the existing files so that crst2s5.py would essentially be identical
+#    to rst2s5.py.   Doing so would mean having folks install essentially
+#    two copies of (some parts of) docutils, which seems a bit of a waste.
+#
+# 2. Use the approach mentioned in 1. above, and use it to create a patch
+#    that would be submitted to the docutils developers, and wait with
+#    fingers crossed for their approval
+#
+# 3. Create a messy hack which relies on the presence of docutils.  This means
+#    less file to be distributed and, if the docutils developers are interested,
+#    it should be fairly simple to port back the changes into the docutils
+#    trunk.  This is the approach we have chosen.
+
+import sys
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import s5_html, html4css1
+
+description = ('Generates S5 (X)HTML slideshow documents from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+class CrunchySlideTranslator(s5_html.S5HTMLTranslator):
+    pass
+
+class CrunchySlideWriter(s5_html.Writer):
+    def __init__(self):
+        html4css1.Writer.__init__(self)
+        self.translator_class = CrunchySlideTranslator
+
+def main():
+    source = sys.argv[-2]
+    destination = sys.argv[-1]
+    writer = CrunchySlideWriter()
+    publish_cmdline(writer=writer, description=description)
+
+if __name__ == '__main__':
+    if ((len(sys.argv) == 2 and sys.argv[1] in ['-h', '--help'])
+        or (
+            len(sys.argv) >= 3 and not sys.argv[-1].startswith("-")
+            and not sys.argv[-2].startswith("-")
+           )
+       ):
+        main()
+    else:
+        print("You must specify files as both source and destination.")
