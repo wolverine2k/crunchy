68f2ed0fc7c967c5a9c024b85179c504eb8feffb
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2009-09-13 19:43:53 +0000

    preparing for release 1.1.2

--- trunk/crunchy/src/plugins/version.py
+++ trunk/crunchy/src/plugins/version.py
@@ -6,7 +6,7 @@ Enable the insertion of Crunchy's version anywhere in a page.
 
 from src.interface import plugin
 
-version = "post 1.1"
+version = "1.1.2"
 
 def register():
     '''registers a simple tag handler'''
