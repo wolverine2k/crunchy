17a0909a834b7aa67cc134e0413a7da1f647ee6b
jdzolonga <jdzolonga@aa0cd930-251a-0410-a94a-f18be9da9e77>
2010-04-11 17:35:11 +0000

    Added pygments as a dependency

--- branches/jdjolonga/crunchy/setup.py
+++ branches/jdjolonga/crunchy/setup.py
@@ -1,4 +1,11 @@
+"""
+:Description: crunchy setup script
+:Author: Josip Djolonga, j.djolonga@jacobs-university.de
+"""
+
 from setuptools import setup, find_packages
+
+# Note: also see the files included by MANIFEST.in
 
 setup(name='crunchy',
       author = 'Andre Roberge',
@@ -8,6 +15,7 @@ setup(name='crunchy',
                'scripts/crunchy-config.py',
                'scripts/crunchy-crst2s5.py'
                ],
+      install_requires = ['pygments'],
       include_package_data = True,
 )
 
