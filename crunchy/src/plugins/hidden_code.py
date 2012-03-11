4444d107f9acb540fb35e47c52d2e4d34743421c
andre.roberge <andre.roberge@aa0cd930-251a-0410-a94a-f18be9da9e77>
2012-03-11 21:43:34 +0000

    Code with hidden parts can be run externally.

--- crunchy/src/plugins/hidden_code.py
+++ crunchy/src/plugins/hidden_code.py
@@ -6,7 +6,7 @@ code samples, replacing them by comment.  Prior to execution, it reintroduces
 those code samples at the appropriate place.
 
 """
-
+import os
 # All plugins should import the crunchy plugin API
 
 # All plugins should import the crunchy plugin API via interface.py
@@ -17,7 +17,7 @@ from src.utilities import wrap_in_div
 requires = set(["editor_widget", "io_widget"])
 
 # each doctest code sample will be kept track via a uid used as a key.
-extracted_lines = {}
+config["extracted_lines"] = {}
 _ = translate['_']
 
 HIDDEN_CODE_MARKER = "# hidden code"
@@ -30,13 +30,16 @@ def register():
        """
     plugin['register_tag_handler']("pre", "title", "hidden_code",
                                           hidden_code_widget_callback)
+    plugin['register_tag_handler']("pre", "title", "hidden_code_external",
+                                          hidden_code_external_widget_callback)
     plugin['register_http_handler'](
                          "/hidden_code%s" % plugin['session_random_id'],
                                        hidden_code_runner_callback)
+    plugin['register_service']("reconstitute_hidden_file", reconstitute_hidden_file)
 
 
 def hidden_code_runner_callback(request):
-    """Handles all execution of hidden_code samples. The request object will contain
+    """Handles execution of hidden_code samples as a thread. The request object will contain
     all the data in the AJAX message sent from the browser."""
     # note how the code to be executed is not simply the code entered by
     # the user, and obtained as "request.data", but also incorporates
@@ -45,25 +48,30 @@ def hidden_code_runner_callback(request):
     if python_version >= 3:
         request.data = request.data.decode('utf-8')
     # add back code here
-    code = request.data
-    _lines = code.split("\n")
-    i = 0
-    for j, _line in enumerate(_lines):
-        if _line == HIDDEN_CODE_MARKER:
-            _lines[j] = extracted_lines[request.args["uid"]][i]
-            i += 1
-    code = "\n".join(_lines)
+    code = reconstitute_hidden_file(request)
 
     plugin['exec_code'](code, request.args["uid"])
     request.send_response(200)
     request.end_headers()
 
 
+def reconstitute_hidden_file(request):
+    code = request.data
+    _lines = code.split("\n")
+    i = 0
+    for j, _line in enumerate(_lines):
+        if _line == HIDDEN_CODE_MARKER:
+            _lines[j] = config["extracted_lines"][request.args["uid"]][i]
+            i += 1
+    code = "\n".join(_lines)
+    return code
+
+
 def hidden_code_widget_callback(page, elem, uid):
     """Handles embedding suitable code into the page in order to display and
     run code samples"""
     vlam = elem.attrib["title"]
-    extracted_lines[uid] = []
+    config["extracted_lines"][uid] = []
     # When a security mode is set to "display ...", we only parse the
     # page, but no Python execution from is allowed from that page.
     # If that is the case, we won't include javascript either, to make
@@ -85,7 +93,7 @@ def hidden_code_widget_callback(page, elem, uid):
         if hide:
             if _line.strip() == HIDDEN_CODE_END:
                 hide = False
-                extracted_lines[uid].append('\n'.join(hidden_lines))
+                config["extracted_lines"][uid].append('\n'.join(hidden_lines))
             else:
                 hidden_lines.append(_line)
         else:
@@ -108,7 +116,6 @@ def hidden_code_widget_callback(page, elem, uid):
     btn.text = _("Show/hide complete code")
     btn.attrib["onclick"] = "$('#%s').toggle()" % ("hidden_pre_" + uid)
 
-
     plugin['services'].insert_editor_subwidget(page, elem, uid, complete_code)
     SubElement(elem, "br")
     btn = SubElement(elem, "button")
@@ -117,6 +124,71 @@ def hidden_code_widget_callback(page, elem, uid):
     btn.attrib["onclick"] = "exec_hidden_code('%s')" % uid
     SubElement(elem, "br")
     plugin['services'].insert_io_subwidget(page, elem, uid)
+
+
+def hidden_code_external_widget_callback(page, elem, uid):
+    """Handles embedding suitable code into the page in order to display and
+    run code samples"""
+    vlam = elem.attrib["title"]
+    config["extracted_lines"][uid] = []
+    # When a security mode is set to "display ...", we only parse the
+    # page, but no Python execution from is allowed from that page.
+    # If that is the case, we won't include javascript either, to make
+    # thus making the source easier to read.
+    if 'display' not in config[page.username]['page_security_level'](page.url):
+        if not page.includes("hidden_code_included"):
+            page.add_include("hidden_code_included")
+            page.add_js_code(hidden_code_jscode)
+
+    elem.attrib['title'] = "py"
+    complete_code, show_vlam = plugin['services'].style(page, elem, None, vlam)
+    #
+    _lines = complete_code.split('\n')
+    displayed_lines = []
+    hidden_lines = []
+
+    hide = False
+    for _line in _lines:
+        if hide:
+            if _line.strip() == HIDDEN_CODE_END:
+                hide = False
+                config["extracted_lines"][uid].append('\n'.join(hidden_lines))
+            else:
+                hidden_lines.append(_line)
+        else:
+            if _line.strip() == HIDDEN_CODE_BEGIN:
+                hide = True
+                hidden_lines = []
+                displayed_lines.append(HIDDEN_CODE_MARKER)
+            else:
+                displayed_lines.append(_line)
+
+    complete_code = '\n'.join(displayed_lines)
+
+    elem.attrib['title'] = vlam
+    elem.attrib["class"] += " hidden"
+    elem.attrib["id"] = "hidden_pre_" + uid
+
+    wrap_in_div(elem, uid, vlam, "hidden_code", show_vlam)
+
+    btn = SubElement(elem, "button")
+    btn.text = _("Show/hide complete code")
+    btn.attrib["onclick"] = "$('#%s').toggle()" % ("hidden_pre_" + uid)
+
+    plugin['services'].insert_editor_subwidget(page, elem, uid, complete_code)
+
+    btn2 = SubElement(elem, "button", id="run_from_file_" + uid)
+    btn2.attrib["onclick"] = "exec_hidden_code_externally('%s')" % uid
+    btn2.text = _("Save and Run from file")
+    path_label = SubElement(elem, "span")
+    path_label.attrib['id'] = 'path_' + uid
+    path_label.attrib['class'] = 'path_info'
+
+    path_label.text = config[page.username]['temp_dir'] + os.path.sep + "temp.py"
+
+    SubElement(elem, "br")
+    plugin['services'].insert_io_subwidget(page, elem, uid)
+
 
 hidden_code_jscode = """
 function exec_hidden_code(uid){
@@ -129,5 +201,14 @@ function exec_hidden_code(uid){
     j.open("POST", "/hidden_code%s?uid="+uid, false);
     j.send(code);
 };
-
-""" % plugin['session_random_id']
+function exec_hidden_code_externally(uid){
+    code=editAreaLoader.getValue("code_"+uid);
+    if (code == undefined) {
+        code = document.getElementById("code_"+uid).value;
+    }
+    path = document.getElementById("path_"+uid).innerHTML;
+    var j = new XMLHttpRequest();
+    j.open("POST", "/save_and_run%s?uid="+uid, false);
+    j.send(path+"_::EOF::_"+code);
+};
+""" % (plugin['session_random_id'], plugin['session_random_id'])
